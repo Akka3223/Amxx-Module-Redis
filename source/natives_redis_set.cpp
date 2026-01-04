67da4a333ca25bee660f91c9ea3f44122339cf88
Akka3223 <58777333+Akka3223@users.noreply.github.com>
2026-01-04 14:25:50 +0200

    implement additional args for redis_set_string & redis_set_integer

--- source/natives_redis_set.cpp
+++ source/natives_redis_set.cpp
@@ -2,32 +2,65 @@
 
 using namespace sw::redis;
 
-// native redis_set_string(const key[], const value[]);
+// native redis_set_string(const key[], const value[], const ttl = 0, const type = 0, const keepttl = 0);
 cell redis_set_string(AMX *amx, cell *params)
 {
-	int len = 0;
+    int len = 0;
+    
 	std::string key   = MF_GetAmxString(amx, params[1], 0, &len);
-	std::string value = MF_GetAmxString(amx, params[2], 1, &len);
+    std::string value = MF_GetAmxString(amx, params[2], 1, &len);
 
-	if (g_redis != NULL)
-		g_redis->set(key, value);
-	else
-		return -1;
+    int ttl = params[3];
+    UpdateType type = (UpdateType)params[4];
+    bool keepttl = (params[5] != 0);
 
-	return 0;
+    if (g_redis != nullptr)
+    {
+        bool success = false;
+
+        if (keepttl)
+        {
+            success = g_redis->set(key, value, keepttl, type);
+        }
+        else
+        {
+            success = g_redis->set(key, value, std::chrono::seconds(ttl), type);
+        }
+
+        return success ? 0 : -1;
+    }
+
+    return -1;
 }
 
-// native redis_set_integer(const key[], const value[]);
+
+// native redis_set_integer(const key[], const value[], const ttl = 0, const type = 0, const keepttl = 0);
 cell redis_set_integer(AMX *amx, cell *params)
 {
-	int len = 0;
-	std::string key   = MF_GetAmxString(amx, params[1], 0, &len);
-	int value = params[2];
+    int len = 0;
 
-	if (g_redis != NULL)
-		g_redis->set(key, std::to_string(value));
-	else
-		return -1;
+	std::string key = MF_GetAmxString(amx, params[1], 0, &len);
+    int value = params[2];
 
-	return 0;
+    int ttl = params[3];
+    UpdateType type = (UpdateType)params[4];
+    bool keepttl = (params[5] != 0);
+
+    if (g_redis != nullptr)
+    {
+        bool success = false;
+
+        if (keepttl)
+        {
+            success = g_redis->set(key, std::to_string(value), keepttl, type);
+        }
+        else
+        {
+            success = g_redis->set(key, std::to_string(value), std::chrono::seconds(ttl), type);
+        }
+
+        return success ? 0 : -1;
+    }
+
+    return -1;
 }
